70281fa7c38d9ad35d1d67091a4888f13463c417
beepbeepimjeep <42129550+beepbeepimjeep@users.noreply.github.com>
2020-09-01 21:32:21 +1000

    connect to mongodb

---
+++ models/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const mongoose = require("mongoose");
+// Connect to MongoDB --- Replace this with your Connection String
+CONNECTION_STRING = "mongodb+srv://zihengt:<password>@cluster0.htzok.mongodb.net/ITProject?retryWrites=true&w=majority";
+MONGO_URL =
+    CONNECTION_STRING.replace("<password>",process.env.MONGO_PASSWORD);
+mongoose.connect(MONGO_URL || "mongodb://localhost/itproject", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    dbName: "ITProject"
+});
+const db = mongoose.connection;
+db.on("error", err => {
+    console.error(err);
+    process.exit(1);
+});
+db.once("open", async () => {
+    console.log("Mongo connection started on " + db.host + ":" +
+        db.port);
+});
+
+require('./usertest')
