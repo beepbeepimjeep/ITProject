4f2c39e503ba18311a9162cbf0b9e68631fb6225
beepbeepimjeep <42129550+beepbeepimjeep@users.noreply.github.com>
2020-09-24 23:40:14 +1000

    file
    display file + delete file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,21 +1,78 @@
 const mongoose  = require("mongoose");
 
-const file = mongoose.model("file")
+const files = mongoose.model("file")
 
-const getAllFile = async (req,res)=>{
-    try {
-        const all_file = await file.find()
-        if(all_file.length==0){
+
+require('dotenv').config()
+const Grid = require('gridfs-stream')
+// Connect to MongoDB --- Replace this with your Connection String
+CONNECTION_STRING = "mongodb+srv://zihengt:<password>@cluster0.htzok.mongodb.net/ITProject?retryWrites=true&w=majority";
+MONGO_URL =
+    CONNECTION_STRING.replace("<password>",process.env.MONGO_PASSWORD);
+mongoose.connect(MONGO_URL || "mongodb://localhost/itproject", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    dbName: "ITProject"
+});
+const db = mongoose.connection;
+db.on("error", err => {
+    console.error(err);
+    process.exit(1);
+});
+let gfs;
+db.once("open", async () => {
+    gfs = Grid(db.db,mongoose.mongo);
+    gfs.collection('upload');
+});
+
+
+const displayImage = (req,res)=>{
+    files.findOne({filename: req.params.filename},(err,file)=>{
+        if(!file||file.length==0){
+            return res.status(404).json({
+                err: 'No File Exist'
+            })
+        }
+        if(file.contentType == 'image/jpeg'||file.contentType == 'img/png'){
+            const readStream = gfs.createReadStream(file.filename);
+            readStream.pipe(res)
+        }else{
             return res.json({
                 err: 'No File Exist'
             })
         }
-        return res.json(all_file);
-    } catch (err) {
-        res.status(400);
-        return res.send("Database query failed");
-    }
-};
+    })
+}
+
+const displayAll = (req,res)=>{
+    gfs.files.find().toArray((err, files)=>{
+        if(!files||files.length==0){
+            res.render('main',{files: false});
+        }else{
+            files.map(file=>{
+                if(file.contentType == 'image/jpeg'||file.contentType == 'image/png'){
+                    file.isImage=true;
+                }else{
+                    file.isImage=false;
+                }
+            })
+            res.render('main',{files:files})
+        }
+    })
+}
+
+const deleteOne = (req,res)=>{
+    gfs.remove({_id: req.params.id, root: 'upload'},(err)=>{
+        if(err){
+            return res.status(404).json({error: err})
+        }
+        res.redirect('/file/main')
+    })
+}
 module.exports = {
-    getAllFile,
+    displayImage,
+    displayAll,
+    deleteOne
 }
