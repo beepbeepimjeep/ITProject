30c958f947f9f7c894df28657cb70ea0b002eaf2
NanyuD-IVY <nanyud@student.unimelb.edu.au>
2020-09-26 23:59:56 +1000

    fuzzy search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,30 +4,32 @@ const user_model = mongoose.model("users")
 
 const getUser = async (req,res)=>{
     try {
-        //need to change "search" according to the query name
-        const users = await user_model.find({userName:req.query.q});
-        //exact matches only
-        //fuzzy search to be implemented
-        const user = users[0];
+        var noMatch = null;
+        if(req.query.q){
+            const regex = new RegExp(escapeRegex(req.query.q), 'gi');
+            user_model.find({userName:regex}, function(err, allUsers){
+                if(err){
+                    console.log(err);
+                }else{
+                    if(allUsers.length < 1){
+                        noMatch = "No users match that query, please try again."
+                    }
+                    res.render("searchResult.ejs", {users:allUsers, noMatch: noMatch});
+                }
+            });
+        }
 
-        if(!user){
-            res.render('resultNotFound.ejs')
-        }
-        else {
-            var result = [];
-            var i = 0;
-            while (users[i]) {
-                result.push(users[i]);
-                i++;
-            }
-            //return res.send(result[0].userName);
-            res.render('searchResult.ejs', {result})
-        }
     } catch (err) {
         res.status(400);
         return res.send("Database query failed");
     }
 };
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+
 module.exports = {
     getUser,
 }
